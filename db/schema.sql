35972a0f0fa62271e3f0897bf8fff2928afce8a8
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-05-02 00:22:07 -0700

    fix table syntax

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employees_db;
+USE employee_db;
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
   id INT NOT NULL PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS position;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
-INSERT INTO department (department_name)
+INSERT INTO department (department_id, department_name)
 VALUES ("Front of house"),
        ("Back of house");
 
-INSERT INTO position (position_id, position)
-VALUES (01, "Manager"),
-       (02, "Chef"),
-       (03, "Waitress"),
-       (04, "Busser"),
-       (05, "Burgersquach"),
-       (06, "Dishwasher");
+INSERT INTO position (position_id, position_title, salary)
+VALUES (01, "Manager", 50000),
+       (02, "Chef", 40000),
+       (03, "Waitress", 30000),
+       (04, "Busser", 20000),
+       (05, "Burgersquach", 20000),
+       (06, "Dishwasher", 20000);
 
-INSERT INTO employee (employee_id, employee)
-VALUES (01, "Bob Belcher"),
-       (02, "Linda Belcher"),
-       (03, "Tina Belcher"),
-       (04, "Gene Belcher"),
-       (05, "Louis Belcher"),
+INSERT INTO employee (employee_id, employee_fn, employee_ln, position_id, manager_id)
+VALUES (01, "Bob", "Belcher", 02, 01),
+       (02, "Linda", "Belcher", 03, 01),
+       (03, "Tina", "Belcher", 06, null),
+       (04, "Gene", "Belcher", 05, null),
+       (05, "Louis", "Belcher", 04, null);
        
-INSERT INTO employees(id, managerId, name) VALUES(01, NULL, 'Bob');
-INSERT INTO employees(id, managerId, name) VALUES(02, NULL, 'Linda');
-INSERT INTO employees(id, managerId, name) VALUES(03, 01, 'Tina');
-INSERT INTO employees(id, managerId, name) VALUES(04, 02, 'Gene');
-INSERT INTO employees(id, managerId, name) VALUES(05, 01, 'Louis');
+INSERT INTO employee(id, managerId, name) VALUES(01, NULL, 'Bob');
+INSERT INTO employee(id, managerId, name) VALUES(02, NULL, 'Linda');
+INSERT INTO employee(id, managerId, name) VALUES(03, 01, 'Tina');
+INSERT INTO employee(id, managerId, name) VALUES(04, 02, 'Gene');
+INSERT INTO employee(id, managerId, name) VALUES(05, 01, 'Louis');
